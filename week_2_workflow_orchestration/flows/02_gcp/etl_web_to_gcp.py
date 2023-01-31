96adcfad5846d061beb9e3af7791ff26e1d31a91
Milovan Minic <milovan.minic@gmail.com>
2023-01-31 21:16:45 +0100

    Introducing the dtype conversion task

--- week_2_workflow_orchestration/flows/02_gcp/etl_web_to_gcp.py
+++ week_2_workflow_orchestration/flows/02_gcp/etl_web_to_gcp.py
@@ -9,12 +9,45 @@ from random import randint
 def fetch(dataset_url: str) -> pd.DataFrame:
     '''Read data from web into Pandas DataFrame'''
 
-    if randint(0, 1) > 0:
-        raise Exception
+    # if randint(0, 1) > 0:
+    #     raise Exception
 
     df = pd.read_csv(dataset_url)
 
     return df
+
+@task(log_prints = True)
+def clean(df = pd.DataFrame) -> pd.DataFrame:
+    '''Fix dtype issues'''
+
+    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+
+    print(df.head(2))
+    print(f'Columns {df.dtypes}')
+    print(f'Count of rows: {len(df)}')
+
+    return df
+
+
+# @task(log_prints=True)
+# def clean(df: pd.DataFrame) -> pd.DataFrame:
+#     """Fix dtype issues"""
+#     df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+#     df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+#     print(df.head(2))
+#     print(f"columns: {df.dtypes}")
+#     print(f"rows: {len(df)}")
+#     return df
+
+
+
+
+
+
+
+
+
 
 
 @flow()
@@ -27,7 +60,7 @@ def etl_web_to_gcs() -> None:
     dataset_url = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz'
 
     df = fetch(dataset_url)
-
+    df_clean = clean(df)
 
 if __name__ == '__main__':
     etl_web_to_gcs()
