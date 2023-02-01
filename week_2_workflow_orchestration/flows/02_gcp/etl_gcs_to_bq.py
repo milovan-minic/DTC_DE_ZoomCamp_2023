2acbfb07c44957d3462ae494753997ff64b9ef18
Milovan Minic <milovan.minic@gmail.com>
2023-02-01 15:48:52 +0100

    Added function that writes data to GBQ into already existing table

--- week_2_workflow_orchestration/flows/02_gcp/etl_gcs_to_bq.py
+++ week_2_workflow_orchestration/flows/02_gcp/etl_gcs_to_bq.py
@@ -27,6 +27,20 @@ def transform(path: Path) -> pd.DataFrame:
 
     return df
 
+@task(retries = 3)
+def write_to_bq(df: pd.DataFrame) -> None:
+    '''Write DataFrame to BigQuery'''
+
+    gcp_credentials_block = GcpCredentials.load("dtc-de-creds")
+
+    df.to_gbq(
+        destination_table = 'dtc_de_bq_dataset_2.yellow_taxi_rides',
+        project_id = 'ace-element-375718',
+        credentials = gcp_credentials_block.get_credentials_from_service_account(),
+        chunksize = 100_000,
+        if_exists = 'append'
+    )
+
 @flow()
 def etl_gcs_to_bq():
     '''Main flow to load data into BigQuery Data Warehouse'''
@@ -36,6 +50,7 @@ def etl_gcs_to_bq():
 
     path = extract_from_gcs(color, year, month)
     df = transform(path)
+    write_to_bq(df)
 
 if __name__ == '__main__':
     etl_gcs_to_bq()
